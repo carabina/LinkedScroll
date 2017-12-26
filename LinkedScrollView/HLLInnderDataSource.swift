11206570c47425a553248a02782acb499f0b8d8a
bochb <bochb@bochbdeiMac.local>
2017-12-26 14:13:58 +0800

    initalize

---
+++ LinkedScrollView/HLLInnderDataSource.swift
@@ -0,0 +1,14 @@
+//
+//  HLLInnderDataSource.swift
+//  Demo
+//
+//  Created by  bochb on 2017/12/22.
+//  Copyright © 2017年 com.heron. All rights reserved.
+//
+
+import UIKit
+
+protocol HLLInnderDataSource: NSObjectProtocol {
+
+    
+}
