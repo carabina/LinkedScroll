11c7d2d48bb15c0e4ee93b5ff2112e40dfb444e9
bochb <bochb@bochbdeiMac.local>
2017-12-26 16:56:43 +0800

    change version

---
+++ LinkedScroll/LinkedScroll.h
@@ -0,0 +1,20 @@
+//
+//  LinkedScroll.h
+//  LinkedScroll
+//
+//  Created by  bochb on 2017/12/26.
+//  Copyright © 2017年 com.heron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LinkedScroll.
+FOUNDATION_EXPORT double LinkedScrollVersionNumber;
+
+//! Project version string for LinkedScroll.
+FOUNDATION_EXPORT const unsigned char LinkedScrollVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LinkedScroll/PublicHeader.h>
+
+
+
